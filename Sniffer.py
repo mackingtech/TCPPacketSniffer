e66c7221836d83be2c525faf5eec702d7b7b5ab4
Mark Dante R. Miranda <mmiran01@rams.shepherd.edu>
2023-04-21 14:54:27 -0400

    for following stream a much better console feedback

--- Sniffer.py
+++ Sniffer.py
@@ -25,6 +25,7 @@ class MainSniffer():
         print(pkt)
         try:
             if TCP in pkt:
+                # Define the data frame that the class would handle based on keys
                 lyst=[
                     [pkt[IP].src, 'ip_src'],
                     [pkt[IP].dst, 'ip_dest'],
@@ -58,43 +59,103 @@ class MainSniffer():
         self.tcp = False
         self.nofilters = False
 
-    def follow_tcp_stream(self, packetlyst, src_port, dst_port, seq_num):
+    def follow_tcp_stream1(self, packetlyst, src_port, dst_port):
         print('Following TCP STREAM')
+        stream_dict = {}
+
+        # group packets by connection
+        for pkt in packetlyst:
+            if pkt.haslayer(TCP):
+                src_ip = pkt[IP].src
+                dst_ip = pkt[IP].dst
+                sport = pkt[TCP].sport
+                dport = pkt[TCP].dport
+                seq = pkt[TCP].seq
+                key = (src_ip, dst_ip, sport, dport)
+
+                # add new connection to dictionary
+                if key not in stream_dict:
+                    stream_dict[key] = []
+                stream_dict[key].append(pkt)
+
+        # sort packets by sequence number and concatenate payloads
+        for key, pkts in stream_dict.items():
+            pkts.sort(key=lambda x: x[TCP].seq)
+            stream = b''
+            for pkt in pkts:
+                stream += bytes(pkt[TCP].payload)
+            print(f'TCP STREAM {key[0]}:{key[2]} -> {key[1]}:{key[3]}')
+            print(stream.decode('utf-8', errors='ignore'))
+
+    def follow_tcp_stream(self, packetlyst, src_port, dst_port, seq_num):
+        print('-------------------------------------------------------------------------------------------------------------------')
+        print('-------------------------------------------------------------------------------------------------------------------')
+        print('-------------------------------------------------------------------------------------------------------------------')
+        print('Following TCP STREAM')
+        print('-------------------------------------------------------------------------------------------------------------------')
+        print('-------------------------------------------------------------------------------------------------------------------')
+        print('-------------------------------------------------------------------------------------------------------------------')
         print(f'{src_port},{dst_port},{seq_num}')
+        exchange = ''
         tcp_packets= list()
-        tcp_payloads= list()
         for pkts in packetlyst:
+            # Filter the based on src/dst port and seq number 
             if str(pkts[TCP].sport) == src_port and str(pkts[TCP].dport) == dst_port and str(pkts[TCP].seq) == seq_num:
-                print(f'STREAM:{str(pkts[TCP].sport)} -----> {str(pkts[TCP].dport)} (SEQ #: {str(pkts[TCP].seq)} )' )
-                tcp_packets.append(pkts)
-                print(pkts[TCP].payload)
+                exchange += str(f'{str(pkts[TCP].sport)} -----> {str(pkts[TCP].dport)} (SEQ #: {str(pkts[TCP].seq)} ) (ACK #:{str(pkts[TCP].ack)})' + '\n')
+                print('-------------------------------------------------------------------------------------------------------------------')
+                print(f'STREAM:{str(pkts[TCP].sport)} -----> {str(pkts[TCP].dport)} (SEQ #: {str(pkts[TCP].seq)} )')
+                print('-------------------------------------------------------------------------------------------------------------------')
+                print('---------------------------------------------------PAYLOAD---------------------------------------------------------')
+                if isinstance(pkts[TCP].payload, NoPayload):
+                    print('No Payload')
+                else:
+                    rawpayload=bytes(pkts[TCP].payload)
+                    print(rawpayload.decode('utf-8', errors='ignore'))
+
+            # reverse the filter so you can filter the acknowledgement packets
+            elif str(pkts[TCP].sport) == dst_port and str(pkts[TCP].dport) == src_port and str(pkts[TCP].ack) == seq_num:
+                exchange += str(f'{str(pkts[TCP].sport)} <----- {str(pkts[TCP].dport)} (SEQ #: {str(pkts[TCP].ack)} )(ACK #:{str(pkts[TCP].seq)})' + '\n')
+                print('-------------------------------------------------------------------------------------------------------------------')
+                print(f'STREAM:{str(pkts[TCP].sport)} <----- {str(pkts[TCP].dport)} (ACKNOWLEDGED SEQ #: {str(pkts[TCP].ack)}) (ACK #:{str(pkts[TCP].seq)})')
+                print('-------------------------------------------------------------------------------------------------------------------')
+                print('---------------------------------------------------PAYLOAD---------------------------------------------------------')
+                if isinstance(pkts[TCP].payload, NoPayload):
+                    print('No Payload')
+                else:
+                    rawpayload=bytes(pkts[TCP].payload)
+                    print(rawpayload.decode('utf-8', errors='ignore'))
+    
+                
 
         #tcp_packets = [pkt for pkt in packetlyst if TCP in pkt and pkt[TCP].sport == src_port and pkt[TCP].dport == dst_port and pkt[TCP].seq == seq_num]
-
-        for i in tcp_packets:
-            payload = i[TCP].payload
-            payload = str(pkts[TCP].sport)+"--->"+str(pkts[TCP].dport)+"--->"+hexstr(payload)
-            tcp_payloads.append(payload)
-        return(tcp_payloads)
+        return exchange
     
     def start(self):
         if self.tcp:
-            sniff(filter="tcp and port "+self.port, count= 20, promisc=True, prn=self.sniffer)
+            if self.port is None:
+                sniff(filter= "tcp", prn = self.sniffer,promisc=True, count= 100)
+            else:
+                sniff(filter="tcp and port "+self.port, count= 20, promisc=True, prn=self.sniffer)
         elif self.udp:
             sniff(filter="udp", prn=self.sniffer, count=10)
 
         elif self.nofilters:
-            sniff(filter= "tcp", prn = self.sniffer, count= 100)
+            sniff(filter= "tcp", prn = self.sniffer, promisc=True, count= 100)
 
 
     
     def get_result(self):
+        '''
         for i in self.item_collection:
             print(i)
+        '''
         return self.item_collection
+        
     
     def getpkt(self):
+        '''
         for i in self.packets:
             print(i)
+        '''
         return self.packets
     
